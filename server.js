cec0441605c8a2557420d2cbe5a31347d9709deb
Harold0823 <harold.loteyro23@gmail.com>
2025-10-25 01:39:04 +0800

    server.js

--- server.js
+++ server.js
@@ -25,4 +25,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
