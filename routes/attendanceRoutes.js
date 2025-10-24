08b3c248126a186bc1b866a0e8ec7a31962d98f7
Harold0823 <harold.loteyro23@gmail.com>
2025-10-24 11:17:38 +0800

    routes/attendanceRoutes.js

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { clockIn, clockOut } from "../controllers/attendanceController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
-router.post("/clockin", clockIn);
-router.post("/clockout", clockOut);
+router.post("/clockin", protect, clockIn);
+router.post("/clockout", protect, clockOut);
 
 export default router;
