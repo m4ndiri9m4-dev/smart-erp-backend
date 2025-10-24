7f3de931d80e853fc131c0f6d5b636f0f118821c
Harold0823 <harold.loteyro23@gmail.com>
2025-10-25 00:49:04 +0800

    routes/authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
+import { login } from "../controllers/authController.js";
+
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/login", login);
 
 export default router;
